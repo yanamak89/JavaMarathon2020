a555923e7353f4bea37c6c8bdd031460b53d37b9
yanamac <yana.mak89@gmail.com>
2020-10-06 09:21:30 +0200

    Day 13 Task1 completed

--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -2,6 +2,26 @@ package day13;
 
 public class Task1 {
     public static void main(String[] args) {
+        User user1 = new User("user1");
+        User user2 = new User("user2");
+        User user3 = new User("user3");
 
+        user1.sendMessage(user2, "Hello from User1");
+        user1.sendMessage(user2, "How are you?");
+
+        user2.sendMessage(user1, "Hej, user1");
+        user2.sendMessage(user1, "I'm user2, nice to meet you.");
+        user2.sendMessage(user1, "I'm fine, and how are you?");
+
+        user3.sendMessage(user1, "Hi, my name is user3");
+        user3.sendMessage(user1, "How are you?");
+        user3.sendMessage(user1, "We need to talk...");
+
+        user1.sendMessage(user3, "Hi, I'm fine");
+        user1.sendMessage(user3, "Are you available tomorrow?");
+
+        user3.sendMessage(user1, "Yes, we can meet tomorrow.");
+
+        MessageDatabase.showDialog(user1, user2);
     }
 }
---
+++ src/test/java/Tests/day3/Day3Task3Test.java
@@ -0,0 +1,18 @@
+//package Tests.day3;
+//
+//import Tests.AbstractTest;
+//import day3.Task3;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//
+//public class Day3Task3Test extends AbstractTest {
+//    @Test
+//    public void ShouldWorkProperly() {
+//        setInput("4 2\n100 0\n13 10\n5 2\n0 0\n");
+//        Task3.main(new String[0]);
+//        assertEquals("2.0\nДеление на 0\n1.3\n2.5\nДеление на 0\n", getOutput(), "4 2     13 10     5 2     0 0");
+//    }
+//
+//}
