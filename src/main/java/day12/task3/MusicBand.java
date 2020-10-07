9f775fec90911e01f3a8a98a9c7f6217683f4e0a
yanamac <yana.mak89@gmail.com>
2020-10-07 09:23:51 +0200

    Updated task3, created separate metod bandsAfter2000

--- src/main/java/day12/task3/MusicBand.java
+++ src/main/java/day12/task3/MusicBand.java
@@ -1,6 +1,7 @@
 package day12.task3;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MusicBand {
@@ -34,6 +35,45 @@ public class MusicBand {
                 "name'" + name + '\'' +
                 ", year=" + year +
                 '}';
+    }
+
+    public static void bandsAfrer2000(){
+        MusicBand band1 = new MusicBand("Barcode Brothers", 1999);
+        MusicBand band2 = new MusicBand("Catamenia", 1995);
+        MusicBand band3 = new MusicBand("Marlango", 2002);
+        MusicBand band4 = new MusicBand("Tesla Boy", 2008);
+        MusicBand band5 = new MusicBand("Scar The Martyr", 2013);
+        MusicBand band6 = new MusicBand("Dio – Distraught Overlord", 2006);
+        MusicBand band7 = new MusicBand("Newsboys", 1985);
+        MusicBand band8 = new MusicBand("Audio Two", 1985);
+        MusicBand band9 = new MusicBand("Garmarna", 1990);
+        MusicBand band10 = new MusicBand("Creed", 1994);
+
+        List<MusicBand> musicBands = new ArrayList<>();
+        musicBands.add(band1);
+        musicBands.add(band2);
+        musicBands.add(band3);
+        musicBands.add(band4);
+        musicBands.add(band5);
+        musicBands.add(band6);
+        musicBands.add(band7);
+        musicBands.add(band8);
+        musicBands.add(band9);
+        musicBands.add(band10);
+
+        System.out.println(musicBands);
+
+        Collections.shuffle(musicBands);
+
+        System.out.println(musicBands);
+
+        List<MusicBand> musicBandsAfter2000 = new ArrayList<>();
+        for (MusicBand band : musicBands) {
+            if (band.getYear() > 2000) {
+                musicBandsAfter2000.add(band);
+            }
+        }
+        System.out.println(musicBandsAfter2000);
     }
 
 }
--- src/main/java/day12/task3/Task3.java
+++ src/main/java/day12/task3/Task3.java
@@ -5,6 +5,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import static day12.task3.MusicBand.bandsAfrer2000;
+
 /**
  * 3. *Выполнять в подпапке task3 в day12*
  * Создать класс Музыкальная Группа (англ. MusicBand) с полями name и year
@@ -22,41 +24,7 @@ import java.util.List;
  */
 public class Task3 {
     public static void main(String[] args) {
-        MusicBand band1 = new MusicBand("Barcode Brothers", 1999);
-        MusicBand band2 = new MusicBand("Catamenia", 1995);
-        MusicBand band3 = new MusicBand("Marlango", 2002);
-        MusicBand band4 = new MusicBand("Tesla Boy", 2008);
-        MusicBand band5 = new MusicBand("Scar The Martyr", 2013);
-        MusicBand band6 = new MusicBand("Dio – Distraught Overlord", 2006);
-        MusicBand band7 = new MusicBand("Newsboys", 1985);
-        MusicBand band8 = new MusicBand("Audio Two", 1985);
-        MusicBand band9 = new MusicBand("Garmarna", 1990);
-        MusicBand band10 = new MusicBand("Creed", 1994);
+        bandsAfrer2000();
 
-        List<MusicBand> musicBands = new ArrayList<>();
-        musicBands.add(band1);
-        musicBands.add(band2);
-        musicBands.add(band3);
-        musicBands.add(band4);
-        musicBands.add(band5);
-        musicBands.add(band6);
-        musicBands.add(band7);
-        musicBands.add(band8);
-        musicBands.add(band9);
-        musicBands.add(band10);
-
-        System.out.println(musicBands);
-
-        Collections.shuffle(musicBands);
-
-        System.out.println(musicBands);
-
-        List<MusicBand> musicBandsAfter2000 = new ArrayList<>();
-        for (MusicBand band : musicBands) {
-            if (band.getYear() > 2000) {
-                musicBandsAfter2000.add(band);
-            }
-        }
-        System.out.println(musicBandsAfter2000);
     }
 }
