6950d379599e7b535717aaa5fa9e102616792e00
yanamac <yana.mak89@gmail.com>
2020-10-08 21:29:57 +0200

    Day 16 Task 1 done.Task 2 cant complete because of Number Format Exception.

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,7 +1,57 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * 1. Реализовать программу, которая на вход принимает txt файл с целыми
+ * числами (можно использовать файл из задания 1 дня 14) и в качестве ответа
+ * выводит в консоль среднее арифметическое этих чисел.
+ * Ответ будет являться вещественным числом.
+ * В консоль необходимо вывести полную запись вещественного числа
+ * (со всеми знаками после запятой) и
+ * сокращенную запись (с 3 знаками после запятой).
+ *
+ *
+ * Детали реализации: В классе Task1 создать публичный статический метод
+ * printResult(File file), в котором реализовать вышеописанную логику.
+ * В методе main() класса Task1 вызвать метод printResult(File file),
+ * передав ему в качестве аргумента объект класса File (txt файл с целыми
+ * числами).
+ * <p>
+ * Пример:
+ * Числа в txt файле: 5 2 8 34 1 36 77
+ * Ответ: 23.285714285714285 --> 23,286
+ */
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        printResult();
+
+    }
+
+    public static void printResult () {
+        File file = new File ("test1");
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            String line = scanner.nextLine();
+            String[] numbers = line.split(" ");
+
+            int sum = 0;
+
+            for (String number : numbers){
+                sum += Integer.parseInt(number);
+            }
+            double result = sum /(double) numbers.length;
+
+            System.out.printf(result + "--> %.3f", result);
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
 
     }
 }
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,80 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.RandomAccess;
+import java.util.Scanner;
+
+/**
+ * 2. Реализовать программу, записывающую числа разных типов в 2 файла.
+ * Файл 1 должен называться “file1.txt”, а Файл 2 должен называться
+ * “file2.txt”.
+ * Оба файла должны находиться в корне проекта. Создаваться файлы
+ * должны не вручную, а при запуске программы.
+ * <p>
+ * <p>
+ * Файл 1 должен содержать 1000 случайных чисел от 0 до 100.
+ * Файл 2 создается на основании Файла 1, но содержит числа
+ * вещественного типа данных.
+ * Метод заполнения Файла 2 следующий: для каждой группы из 20 целых
+ * чисел из Файла 1 рассчитывается их среднее арифметическое. Затем,
+ * полученное значение записывается в Файл 2. Таким образом в Файле 2
+ * будет находиться 50 вещественных чисел (1000 / 20 = 50).
+ * После того, как Файл 2 будет сформирован, необходимо реализовать
+ * статический метод printResult(File file). Этот метод должен
+ * рассчитать сумму всех вещественных чисел из Файла 2 и вывести её в
+ * консоль, отбросив вещественную часть.
+ * <p>
+ * Пример:
+ * Для простоты, в Файле 1 находится 12 целых чисел, а среднее
+ * арифметическое рассчитывается для группы из 4 целых чисел.
+ * <p>
+ * Файл 1: 3 8 7 5 28 73 4 1 5 32 89 12
+ * Файл 2: 5.75 26.5 34.5
+ * Ответ: 66
+ */
 public class Task2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException{
+        writeToTheFile();
+
+        //Выбрасывает исключение NumberFormatException, не знаю как его пофиксить
+        printResult();
+
+    }
+
+
+        public static void writeToTheFile() throws FileNotFoundException{
+            Random random = new Random();
+            File file = new File("file1.txt");
+            PrintWriter pw = new PrintWriter(file);
+
+            for (int i = 0; i < 1000; i++) {
+                pw.println(random.nextInt(100));
+            }
+            pw.close();
+
+        }
+
+        public static void printResult() throws FileNotFoundException{
+        Scanner scanner = new Scanner("file1.txt");
+        File file2 = new File("file2.txt");
+        PrintWriter pw2 = new PrintWriter(file2);
+        int counter = 0;
+        int sum = 0;
+        while (scanner.hasNextLine()) {
+            sum += Integer.parseInt(scanner.nextLine());
+            counter++;
+            if (counter == 20) {
+                pw2.println(sum / 20.0);
+
+                counter = 0;
+                sum = 0;
+            }
+        }
+
+        pw2.close();
 
     }
 }
